b9faba5754497fe6fb983cbf70ccaeb6a5c7f2c8
Tony <tony.ta.sanchez@gmail.com>
2026-01-30 14:22:55 -0500

    add ms status service

---
+++ yape-challange/status-service/src/main/java/com/example/statusservice/StatusServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.statusservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatusServiceApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(StatusServiceApplication.class, args);
+    }
+}
---
+++ yape-challange/status-service/src/main/java/com/example/statusservice/dto/TransactionType.java
@@ -0,0 +1,12 @@
+package com.example.statusservice.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TransactionType {
+    private String name;
+}
